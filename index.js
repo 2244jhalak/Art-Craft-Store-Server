c57b7457aeac1958fd2f52cdd80d0798cacdd5d1
Jhalak <stdjhalak@gmail.com>
2024-04-27 01:13:39 +0600

    data find with email functionality added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express();
@@ -39,6 +39,16 @@ async function run() {
         const cursor = craftCollection.find();
         const result = await cursor.toArray();
         res.send(result);
+    })
+    app.get('/craft/:email',async(req,res)=>{
+        const email=req.params.email;
+        const query={User_Email : email };
+        // const result= await craftCollection.findOne(query);
+        const cursor = craftCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+  
+  
     })
 
     app.post('/craft',async(req,res)=>{
